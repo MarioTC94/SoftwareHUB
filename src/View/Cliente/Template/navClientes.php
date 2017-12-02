ae946f10b295e14bc6000a5c7955e14ef14a7890
DanielaTorresF12 <33816721+DanielaTorresF12@users.noreply.github.com>
2017-12-02 04:19:51 -0600

    modificacion del nanvar y una nueva pagina

--- src/View/Cliente/Template/navClientes.php
+++ src/View/Cliente/Template/navClientes.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default">
 <div class="container contenedor-navbar">
-   <div class="navbar-header">
+   <div class="navbar-header">s
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1">
             <span class="sr-only">Alternar Menu</span>
             <span class="icon-bar"></span>
